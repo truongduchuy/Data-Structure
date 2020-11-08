e5f8b82592f6baa1e82b6a46674767e277d05669
Huy Truong <huy.truong@mevry.com>
2020-11-08 22:16:22 +0700

    push & pop singly linked list

---
+++ SinglyLinkedList/index.js
@@ -0,0 +1,53 @@
+class Node {
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+class SinglyLinkedList{
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        var newNode = new Node(val);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        }
+        else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        var current = this.head;        
+        if(!current)
+            return undefined;
+        var newTail = current;
+
+        while(current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+}
+
+var list = new SinglyLinkedList()
+list.push(1)
+list.push(2)
+list.push(3)
